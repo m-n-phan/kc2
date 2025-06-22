0efcf5ca2f15a3104d7aaeaa2638d13d4a83bebd
m-n-phan <mphan1257@gmail.com>
2025-06-21 20:57:02 -0500

    Add dotenv config and environment docs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import trainingRoutes from './routes/training'
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
